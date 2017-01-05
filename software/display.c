3c6cda3ecb86bdfa0ed8e0d104e980baa380afae
John C. Lemme <jclemme@proportionallabs.com>
2017-01-05 09:03:07 -0500

    Added roadmap for display controller

--- software/display.c
+++ software/display.c
@@ -2,4 +2,14 @@
 
 static volatile uint8_t pinSDA, pinSCL;
 
+int Display_begin(uint8_t sda, uint8_t scl)
+{
+    
+}
+
+int Display_end()
+{
+
+}
+
 
--- software/display.h
+++ software/display.h
@@ -6,3 +6,18 @@
 #include <string.h>
 
 #define TRS_DELAY (_CLKFREQ >> 5)
+
+/*
+
+What this needs to do:
+
+* Reset/clear the display
+* Allow editing of framebuffer directly
+* Blit graphics in memory to display
+* Print text on display
+* Scroll display
+* Cog to update from framebuffer
+
+*/
+
+
